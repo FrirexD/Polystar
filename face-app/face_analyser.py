b136e3a76766d0bc56857876d3411df61ae9ace2
Agheroo <reauxrenan@outlook.fr>
2025-01-29 10:02:16 +0100

    Small fix of matching samples

--- face-app/face_analyser.py
+++ face-app/face_analyser.py
@@ -6,7 +6,6 @@ from utils import detect_and_draw_faces, extract_embedding
 from constants import *
 import pickle
 import os
-from morphing import morph_images
 
 def initialize_face_analyzer(model:str = 'buffalo_l') -> FaceAnalysis:
     """
@@ -130,10 +129,10 @@ def main(source_image_path):
     detected_faces_img , _= detect_and_draw_faces(source_image_path,OUTPUT_DIR, "detected_faces.jpg")
     cv.imwrite(OUTPUT_DIR+"detected_faces.jpg",detected_faces_img)
 
-    comparing_image_path = SAMPLES_DIR+"Renan1.JPG"
-    compare_embedding = extract_embedding(comparing_image_path, app)
-    compared_images = visualize_match(source_image_path, comparing_image_path, calculate_similarity(source_embedding, compare_embedding))
-    cv.imwrite(OUTPUT_DIR+"matching_samples.jpg",compared_images)
+    #comparing_image_path = SAMPLES_DIR+"Renan1.JPG"
+    #compare_embedding = extract_embedding(comparing_image_path, app)
+    #compared_images = visualize_match(source_image_path, comparing_image_path, calculate_similarity(source_embedding, compare_embedding))
+    #cv.imwrite(OUTPUT_DIR+"matching_samples.jpg",compared_images)
 
     # Find best score of matching star with source
     matching_image_path, score = find_best_match(source_embedding)
